86558b89e03b6a361093a4cf54071f3545a6a067
Danyil Lytvishko <birion.red@gmail.com>
2024-12-09 21:47:35 +0100

    player HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,12 @@
+// Copyright CyberFrog
+
+
+#include "UI/HUD/AuraHUD.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}

eda117f8f1d1d3d995ac298425cec5e70f664362
Danyil Lytvishko <birion.red@gmail.com>
2025-03-22 10:54:29 +0100

    Add native gameplay tags

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -2,11 +2,31 @@
 
 
 #include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
 
-AuraGameplayTags::AuraGameplayTags()
-{
-}
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
 
-AuraGameplayTags::~AuraGameplayTags()
+void FAuraGameplayTags::InitNativeGameplayTags()
 {
+	/*
+	 * Primary attributes
+	 */
+	GameplayTags.Attributes_Primary_Strenght = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Strenght"), FString("Increase physical and critical damage"));
+	GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Intelligence"), FString("Increase magical damage and mana capacity"));
+	GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Resilience"), FString("Increase defence and critical chance"));
+	GameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Vigor"), FString("Increase HP"));
+
+	/*
+	 * Secondary attributes
+	 */
+	GameplayTags.Attributes_Secondary_CritDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CritDamage"), FString("Critical hit damage"));
+	GameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.MaxMana"), FString("Maximum mana"));
+	GameplayTags.Attributes_Secondary_ManaRegen = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.ManaRegen"), FString("Mana regeneration"));
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.ArmorPenetration"), FString("Increase damage dealt to enemies"));
+	GameplayTags.Attributes_Secondary_CritChance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CritChance"), FString("Increase critical hit chance"));
+	GameplayTags.Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.BlockChance"), FString("Increase chance to block damage"));
+	GameplayTags.Attributes_Secondary_CritResist = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CritResist"), FString("Reduce enemy critical hit chance"));
+	GameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.MaxHealth"), FString("Maximum health"));
+	GameplayTags.Attributes_Secondary_HealthRegen = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.HealthRegen"), FString("Health regeneration"));
 }
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -3,13 +3,41 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 
 /**
- * 
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
  */
-class AURA_API AuraGameplayTags
+
+struct FAuraGameplayTags
 {
 public:
-	AuraGameplayTags();
-	~AuraGameplayTags();
+	static const FAuraGameplayTags& Get() { return GameplayTags ;} 
+	static void InitNativeGameplayTags();
+
+	//Primary
+	FGameplayTag Attributes_Primary_Strenght;
+	FGameplayTag Attributes_Primary_Intelligence;
+	FGameplayTag Attributes_Primary_Resilience;
+	FGameplayTag Attributes_Primary_Vigor;
+
+	//Secondary
+	FGameplayTag Attributes_Secondary_CritDamage;
+	FGameplayTag Attributes_Secondary_MaxMana;
+	FGameplayTag Attributes_Secondary_ManaRegen;
+	FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_CritChance;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CritResist;
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_HealthRegen;
+protected:
+	
+private:
+	static FAuraGameplayTags GameplayTags;
+
+	
 };
